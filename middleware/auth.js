650127b264e148f9e624fcae9a73b68fcd179dc0
unknown <guzma087@cougars.csusm.edu>
2020-01-04 16:47:54 -0800

    adding token capabilities still working some issues..

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,2 +1,19 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
+
+module.exports = function(req, res, next) {
+  //Get token fromheader
+  const token = req.header("x-auth-token");
+
+  //check if not token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+  //verify token
+  try {
+    const decoded = jwt.verfiy(token, config.get("securedtoken"));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {}
+};
